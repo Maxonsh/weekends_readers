8014a80ab3409b9f78a5f474f669f9a5daf3658e
Alexandr Borisov <aishek@gmail.com>
2015-10-15 12:57:17 +0300

    подправлен стиль кода

--- db/migrate/20151013151915_create_letters.rb
+++ db/migrate/20151013151915_create_letters.rb
@@ -1,7 +1,6 @@
 class CreateLetters < ActiveRecord::Migration
   def change
     create_table :letters do |t|
-
       t.timestamps null: false
     end
   end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,10 +2,20 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+
+require 'shoulda/matchers'
+::Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+
+    with.library :active_record
+    with.library :active_model
+  end
+end
 
 require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
