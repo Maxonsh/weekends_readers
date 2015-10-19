d8b4c7da02767cbc69966b77257ebfe3ad45f0f9
Максим Шайковский <maxim@cifronomika.ru>
2015-10-19 23:02:48 +0300

    Начало разработки модели Letter

--- app/models/letter.rb
+++ app/models/letter.rb
@@ -1,2 +1,11 @@
 class Letter < ActiveRecord::Base
+
+  validates :content, presence: true
+  validates :position, uniqueness: true
+
+  scope :queue, -> { order(position: :desc) }
+
+  def self.for_send
+    Letter.where(:status => '').queue.last
+  end
 end
