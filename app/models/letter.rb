de250a28bb6e39bf35a26029eb9bbbce2348e4e2
Максим Шайковский <maxim@cifronomika.ru>
2015-10-20 18:41:39 +0300

    кажется модель готова

--- app/models/letter.rb
+++ app/models/letter.rb
@@ -4,8 +4,18 @@ class Letter < ActiveRecord::Base
   validates :position, uniqueness: true
 
   scope :queue, -> { order(position: :desc) }
+  scope :draft, -> { where(:status => 'draft') }
+
+  state_machine :status, :initial => :draft do
+    state :draft, :value => 'draft'
+    state :sent, :value => 'sent'
+
+    event :to_send do
+      transition :draft => :sent
+    end
+  end
 
   def self.for_send
-    Letter.where(:status => '').queue.last
+    Letter.draft.queue.last
   end
 end
