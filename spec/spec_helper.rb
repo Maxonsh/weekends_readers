6ff5c9cdd0590455d762058ec75f9e7105d01d13
Максим Шайковский <maxim@cifronomika.ru>
2015-10-22 10:34:52 +0300

    исправил запуск тестов

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'bundler/setup'
 ::Bundler.require(:default, :test)
 
+ENV['RAILS_ENV'] ||= 'test'
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
