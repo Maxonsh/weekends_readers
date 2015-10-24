50232db77d8684d1e6738f1a074e6be7178b12c9
Максим Шайковский <maxim@cifronomika.ru>
2015-10-24 16:12:09 +0300

    написал тест контроллера

--- spec/controllers/letter_controller_spec.rb
+++ spec/controllers/letter_controller_spec.rb
@@ -1,44 +1,74 @@
 require 'rails_helper'
 
 describe LetterController do
+  let(:content) { Faker::Lorem.sentences }
+
   render_views
 
-  describe 'GET "index"' do
+  describe 'GET #index' do
     it 'return http success' do
-      xhr :get, :index, :format => :js
+      get :index
       expect(response).to be_success
     end
   end
 
-  describe 'GET "new"' do
+  describe 'GET #new' do
     it 'return http success' do
-      xhr :get, :new, :format => :js
+      get :new
       expect(response).to be_success
     end
   end
 
-  describe 'POST "create"' do
-    it 'создалось письмо и произошел редирект на new' do
-      expect(response).to redirect_to("/401.html")
+  describe 'POST #create' do
+    subject { post :create, letter: { content: content } }
+
+    it 'создалось письмо' do
+      Letter.delete_all
+      subject
+      expect(Letter.find_by content: content).to be_present
+    end
+
+    it 'произошел редирект на new' do
+      expect(subject).to redirect_to(new_letter_path)
     end
   end
 
-  describe 'GET "edit"' do
+  describe 'GET #edit' do
     it 'return http success' do
-      xhr :get, :edit, :format => :js
+      get :edit
       expect(response).to be_success
     end
   end
 
-  describe 'PUT "update"' do
-    it 'поменялся контент письма и произошел редирект на index' do
+  describe 'PUT #update' do
+    let(:content1) { Faker::Lorem.sentences }
+    let(:content2) { Faker::Lorem.sentences }
 
+    letter = Letter.create! content: content1
+
+    subject { put :update, letter: { content: content2 }, id: letter.id }
+
+    it 'поменялся контент письма' do
+      subject
+      expect(letter.content).to eq(content2)
+    end
+    it 'произошел редирект на index' do
+      expect(subject).to redirect_to(letter_index_path)
     end
   end
 
-  describe 'DELETE "destroy"' do
-    it 'письмо удалилось и произошел редирект на index' do
+  describe 'DELETE #destroy' do
+    letter = Letter.create! content: content
 
+    subject { delete :destroy, id: letter.id }
+
+    it 'письмо удалилось' do
+      subject
+      expect(letter).not_to exist
+    end
+
+    it 'произошел редирект на index' do
+      expect(subject).to redirect_to(letter_index_path)
     end
   end
 end
