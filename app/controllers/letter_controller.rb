e6757d1f5a360e144d69add8259447d7e05c42d1
Максим Шайковский <maxim@cifronomika.ru>
2015-10-26 18:55:03 +0300

    начало контроллера

---
+++ app/controllers/letter_controller.rb
@@ -0,0 +1,29 @@
+class LetterController < ApplicationController
+
+  def index
+    @queue = Letter.queue
+  end
+
+  def new
+    @letter = Letter.new
+  end
+
+  def create
+    @letter = Letter.new(params[:letter])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def letter_params
+    params.require(:letter).permit(:content, :status, :position)
+  end
+end
