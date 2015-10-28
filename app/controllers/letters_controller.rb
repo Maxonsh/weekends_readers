3ba5b537d1c4c29e30d63e5bee7eef56fff71580
Максим Шайковский <maxim@cifronomika.ru>
2015-10-28 17:45:39 +0300

    Готов letter_controller и сопутствующее

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -1,43 +1,54 @@
 class LettersController < ApplicationController
+  helper_method :resource_letter
 
   def index
     @queue = Letter.queue
   end
 
   def new
-    @letter = Letter.new
+    @resource_letter = Letter.new
   end
 
   def create
-    @letter = Letter.new(letter_params)
+    @resource_letter = Letter.new(letter_params)
 
-    if @letter.save!
+    if @resource_letter.save
       redirect_to new_letter_path
+      flash[:notice] = 'Письмо добавлено в очередь'
     else
       render 'edit'
+      flash[:notice] = 'Не удалось создать письмо'
     end
   end
 
   def edit
-    @letter = Letter.find(params[:id])
   end
 
   def update
-    @letter = Letter.find(params[:id])
-
-    if @letter.update(letter_params)
+    if resource_letter.update(letter_params)
       redirect_to letters_path
+      flash[:notice] = 'Письмо обновлено'
     else
       render 'edit'
+      flash[:notice] = 'Не удалось обновить письмо'
     end
   end
 
   def destroy
-    @letter = Letter.find(params[:id])
-    redirect_to letters_path if @letter.destroy
+    if resource_letter.destroy
+      redirect_to letters_path
+      flash[:notice] = 'Письмо удалено'
+    else
+      redirect_to letters_path
+      flash[:notice] = 'Не удалось удалить письмо'
+    end
   end
 
   private
+
+  def resource_letter
+    @resource_letter ||= Letter.find(params[:id])
+  end
 
   def letter_params
     params.require(:letter).permit(:content, :status, :position)
--- spec/controllers/letters_controller_spec.rb
+++ spec/controllers/letters_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe LettersController do
+  let(:content) { Faker::Lorem.paragraph }
 
   render_views
 
@@ -19,7 +20,6 @@ describe LettersController do
   end
 
   describe 'POST #create' do
-    let!(:content) { Faker::Lorem.sentences }
     subject { post :create, letter: { content: content } }
 
     it 'создалось письмо' do
@@ -34,15 +34,17 @@ describe LettersController do
   end
 
   describe 'GET #edit' do
+    let(:letter) { Letter.create! content: content }
+
     it 'return http success' do
-      get :edit
+      get :edit, id: letter.id
       expect(response).to be_success
     end
   end
 
   describe 'PUT #update' do
-    let(:content1) { Faker::Lorem.sentences }
-    let(:content2) { Faker::Lorem.sentences }
+    let(:content1) { Faker::Lorem.paragraph }
+    let(:content2) { Faker::Lorem.paragraph }
     let(:letter) { Letter.create! content: content1 }
 
     subject { put :update, letter: { content: content2 }, id: letter.id }
@@ -58,14 +60,13 @@ describe LettersController do
   end
 
   describe 'DELETE #destroy' do
-    let(:content) { Faker::Lorem.sentences }
     let(:letter) { Letter.create! content: content }
 
     subject { delete :destroy, id: letter.id }
 
     it 'письмо удалилось' do
       subject
-      expect(letter.reload).not_to be_present
+      expect { letter.reload }.to raise_error(ActiveRecord::RecordNotFound)
     end
 
     it 'произошел редирект на index' do
