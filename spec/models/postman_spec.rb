a41540bfa00bc7618e829654c0aa4c472bfc6704
Максим Шайковский <maxim@cifronomika.ru>
2015-10-29 23:09:35 +0300

    дополнение

--- spec/models/postman_spec.rb
+++ spec/models/postman_spec.rb
@@ -4,9 +4,25 @@ describe Postman do
   let(:content) { Faker::Lorem.paragraph }
   let(:letter) { Letter.create! content: content }
 
-  it 'отправляет письмо' do
-    expect { described_class.deliver letter }.to change { ActionMailer::Base.deliveries.count }.by(1)
-  end
+  describe 'отправляет письмо' do
+    let(:last_email) { ActionMailer::Base.deliveries.last }
+    
+    it 'письмо отправлено' do
+      expect { described_class.deliver letter }.to change { ActionMailer::Base.deliveries.count }.by(1)
+    end
+    
+    it 'адресат совпадает' do
+      expect(last_email.to).to eq ['staff@cifronomika.ru']
+    end 
+    
+    it 'тема совпадает' do
+      expect(last_email.subject).to eq ['Что почитать на выходных']
+    end  
+    
+    it 'содержание совпадает' do
+      expect(last_email.body.raw_source).to eq(content)
+    end   
+  end  
 
   it 'статус отправленого письма изменен' do
     expect(letter.reload).to be_sent
