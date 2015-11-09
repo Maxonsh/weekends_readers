061713b0fcd72e68f86ab6e2a9f5b77ffd08071d
Максим Шайковский <maxim@cifronomika.ru>
2015-11-09 13:32:48 +0300

    добавлено кэширование и правки по замечаниям

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -4,44 +4,57 @@ class LettersController < ApplicationController
 
   def index
     @queue = Letter.queue
+    fresh_when cache_key_with_flash(@queue)
   end
 
   def new
     @resource_letter = Letter.new
+    fresh_when cache_key_with_flash(@resource_letter)
   end
 
   def create
     @resource_letter = Letter.new(letter_params)
 
     if @resource_letter.save
-      redirect_to new_letter_path, notice: t('.notice')
+      redirect_to new_letter_path, notice: t('.created')
     else
-      flash.now[:error] = t('.error')
+      flash.now[:error] = t('.not_created')
       render 'new'
     end
   end
 
   def edit
+    fresh_when cache_key_with_flash(resource_letter)
   end
 
   def update
     if resource_letter.update(letter_params)
-      redirect_to letters_path, notice: t('.notice')
+      redirect_to letters_path, notice: t('.updated')
     else
-      flash.now[:error] = t('.error')
+      flash.now[:error] = t('.not_updated')
       render 'edit'
     end
   end
 
   def destroy
     if resource_letter.destroy
-      redirect_to letters_path, notice: t('.notice')
+      redirect_to letters_path, notice: t('.destroyed')
     else
-      redirect_to letters_path, error: t('.error')
+      redirect_to letters_path, error: t('.not_destroyed')
     end
   end
 
   private
+
+  def cache_key_with_flash(record_or_relation)
+    cache_key = if record_or_relation.respond_to?(:cache_key)
+                  record_or_relation.cache_key
+                else
+                  record_or_relation.map(&:cache_key).join('/')
+                end
+
+    "#{cache_key}/#{flash.to_hash}"
+  end
 
   def authenticate
     authenticate_or_request_with_http_basic do |username, password|
