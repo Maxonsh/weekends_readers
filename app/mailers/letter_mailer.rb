7dc1845b1f2d1bc3f0317430d217d730dc8966be
Максим Шайковский <maxim@cifronomika.ru>
2015-11-23 10:56:01 +0300

    вернул адрес для рассылки

--- app/mailers/letter_mailer.rb
+++ app/mailers/letter_mailer.rb
@@ -2,7 +2,7 @@ class LetterMailer < ApplicationMailer
   def send_mail(letter)
     @letter = letter
     mail(
-      to: 'ab@cifronomika.ru',
+      to: 'staff@cifronomika.ru',
       subject: 'Что почитать на выходных'
     )
   end
